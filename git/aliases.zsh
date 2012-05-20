029215965cc459712c51ef03c017114d8cf2c292
Andrew Stewart <andrew@averagestudios.com>
2012-05-20 11:55:32 -0700

    Improved look of glog slightly

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,4 +4,4 @@ alias git='hub'
 # the rest of the fun aliases
 alias gs='git status -sb'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %Cgreen(%cr)%Creset %an: %s%Creset %C(yellow)%d%Creset' --abbrev-commit --date=relative"
