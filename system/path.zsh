8ee4cbb2852854dc85b22193de4b9f0cec081ea4
Andrew Stewart <andrew@averagestudios.com>
2012-08-10 15:04:46 -0700

    Switching up path

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH=".:/usr/local/bin:/usr/local/lib/node_modules:/usr/local/sbin:/usr/bin:/bin:$ZSH/bin:$PATH"
+export PATH=".:$ZSH/bin:/usr/local/bin:/usr/local/lib/node_modules:/usr/local/sbin:/usr/bin:/bin:$PATH"
