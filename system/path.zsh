fc17f2fcf27cdecb0df03711038177b982ebf102
Andrew Stewart <andrew@averagestudios.com>
2012-09-16 19:02:45 -0700

    Way of the future

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH=".:$ZSH/bin:/usr/local/bin:/usr/local/lib/node_modules:/usr/local/sbin:/usr/bin:/bin:$PATH"
+export PATH=".:$ZSH/bin:/usr/local/bin:/usr/local/share/npm/bin:/usr/local/sbin:/usr/bin:/bin:$PATH"
