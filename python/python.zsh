48e9534c73ec1e191612f51dfcda8796f94c63ef
Andrew Stewart <andrew@averagestudios.com>
2012-10-13 10:32:02 -0700

    Python support

---
+++ python/python.zsh
@@ -0,0 +1,6 @@
+export VIRTUALENV_LOCATION=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+export PIP_VIRTUALENV_BASE=$VIRTUALENV_LOCATION
+
+# virtualenvwrapper
+source /usr/local/share/python/virtualenvwrapper.sh
--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH=".:$ZSH/bin:/usr/local/bin:/usr/local/share/npm/bin:/usr/local/sbin:/usr/bin:/bin:$PATH"
+export PATH=".:$ZSH/bin:/usr/local/bin:/usr/local/share/npm/bin:/usr/local/sbin:/usr/bin:/bin:/usr/local/share/python:$PATH"
