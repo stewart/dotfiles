47df25d2ffa0d3e062bec1f5009e8ae8a4c83d44
Andrew Stewart <andrew@averagestudios.com>
2012-10-19 20:18:17 -0700

    Added PYTHONPATH because Django

--- python/python.zsh
+++ python/python.zsh
@@ -2,5 +2,7 @@ export VIRTUALENV_LOCATION=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
 export PIP_VIRTUALENV_BASE=$VIRTUALENV_LOCATION
 
+export PYTHONPATH=/usr/local/lib/python2.7/site-packages
+
 # virtualenvwrapper
 source /usr/local/share/python/virtualenvwrapper.sh
