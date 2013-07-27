69714b7911a9041fa90cb4a5c3f66633c10d2534
Andrew Stewart <andrew@stwrt.ca>
2013-07-26 20:54:41 -0700

    Add ./bin to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH=".:$ZSH/bin:/usr/local/heroku/bin:/usr/local/bin:/usr/local/share/npm/bin:/usr/local/sbin:/usr/bin:/bin:/usr/local/share/python:$PATH"
+export PATH=".:./bin:$ZSH/bin:/usr/local/heroku/bin:/usr/local/bin:/usr/local/share/npm/bin:/usr/local/sbin:/usr/bin:/bin:/usr/local/share/python:$PATH"
