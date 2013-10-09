824ed91ec6896ad84b247d159db68cfce0de4786
Andrew Stewart <andrew@stwrt.ca>
2013-10-08 23:15:17 -0700

    Remove rprompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,17 +8,8 @@ export PROMPT='%{$fg[green]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_col
 
 export GIT_PROMPT_PREFIX=" %{$fg[blue]%}on%{$reset_color%} %{$fg_bold[red]%}"
 export GIT_PROMPT_SUFFIX="%{$reset_color%}"
-export GIT_PROMPT_DIRTY=""
+export GIT_PROMPT_DIRTY="*"
 export GIT_PROMPT_CLEAN=""
-
-export RPROMPT='${return_status} $(git_prompt_status) %{$reset_color%}'
-
-export GIT_PROMPT_ADDED="%{$fg[green]%} ✚"
-export GIT_PROMPT_MODIFIED="%{$fg[blue]%} ✹"
-export GIT_PROMPT_DELETED="%{$fg[red]%} ✖"
-export GIT_PROMPT_RENAMED="%{$fg[magenta]%} ➜"
-export GIT_PROMPT_UNMERGED="%{$fg[yellow]%} ═"
-export GIT_PROMPT_UNTRACKED="%{$fg[cyan]%} ✭"
 
 rb_prompt(){
   if [[ $(which ruby) == "/usr/bin/ruby" ]]
