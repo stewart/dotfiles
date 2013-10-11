5b9b705bcbd6c001649c58d73195662c0bff2c4d
Andrew Stewart <andrew@stwrt.ca>
2013-10-10 22:38:33 -0700

    Fix formatting

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,7 +4,7 @@ export MODE_INDICATOR="%{$fg_bold[red]%}❮%{$reset_color%}%{$fg[red]%}❮❮%{$
 local return_status="%{$fg[red]%}%(?..⏎)%{$reset_color%}"
 
 export PROMPT='%{$fg[green]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg[cyan]%}%c$(git_prompt_info) %{$reset_color%}$(rb_prompt)
-%(?.%F{$fg_bold[green]%}.%F{red})λ%f '
+%(?.%{$fg_bold[green]%}.%{$fg[red]%})λ %{$reset_color%}'
 
 export GIT_PROMPT_PREFIX=" %{$fg[blue]%}on%{$reset_color%} %{$fg_bold[red]%}"
 export GIT_PROMPT_SUFFIX="%{$reset_color%}"
