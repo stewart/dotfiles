d0d44f315ed1503d7e4c28d5757340af24df4e23
Andrew Stewart <andrew@stwrt.ca>
2013-11-04 20:58:08 -0800

    more selecta

--- selecta/functions.zsh
+++ selecta/functions.zsh
@@ -9,7 +9,7 @@ function tsa() {
 # Vim-Find
 # Quickly find and open a file in vim
 function vif() {
-  vim `find . -type f -or -type l -maxdepth 15 | sort -u | selecta`
+  vim `find * -type f -or -type l -maxdepth 15 | sort -u | selecta`
 }
 
 # Kill-Process
