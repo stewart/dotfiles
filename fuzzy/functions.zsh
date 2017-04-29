8a00e877569ad583b0cffa3625395f40af451aa0
Andrew Stewart <andrew@stwrt.ca>
2017-04-28 19:08:09 -0700

    I apparently use rff now

--- fuzzy/functions.zsh
+++ fuzzy/functions.zsh
@@ -3,23 +3,23 @@
 # tmux-select-attach
 # Quickly find and attach to an open tmux session
 function tsa() {
-  tmux attach -t `tmux ls | sort | cut -f1 -d: | fzy`
+  tmux attach -t `tmux ls | sort | cut -f1 -d: | rff`
 }
 
 # Vim-Find
 # Quickly find and open a file in vim
 function v() {
-  vim `find * -maxdepth 15 -type f -or -type l | ag -v '^(node_modules|vendor/bundle|bower_components|_build|tmp|.git)' | fzy`
+  vim `find * -maxdepth 15 -type f -or -type l | ag -v '^(node_modules|vendor/bundle|bower_components|_build|tmp|.git)' | rff`
 }
 
 # Kill-Process
 # Find and kill a process
 function kps() {
-  kill `ps aux | fzy | awk '{print $2 }'`
+  kill `ps aux | rff | awk '{print $2 }'`
 }
 
 # Git-Select-Branch
 # Checkout a branch
 function gsb() {
-  git branch --all --remotes | cut -c 3- | rev | cut -d "/" -f 1 | rev | fzy | xargs git checkout
+  git branch --all --remotes | cut -c 3- | rev | cut -d "/" -f 1 | rev | rff | xargs git checkout
 }
