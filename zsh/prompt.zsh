c381b2aa5584daf8d8bc4b1ffbcfdb8cadee3c40
Andrew Stewart <andrew@stwrt.ca>
2019-02-16 16:07:41 -0800

    Compress prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,10 +3,10 @@ autoload colors && colors
 export MODE_INDICATOR="%{$fg_bold[red]%}❮%{$reset_color%}%{$fg[red]%}❮❮%{$reset_color%}"
 local return_status="%{$fg[red]%}%(?..⏎)%{$reset_color%}"
 
-export PROMPT='%{$fg[green]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg[cyan]%}%c$(git_prompt_info) %{$reset_color%}
-%(?.%{$fg_bold[green]%}.%{$fg[red]%})λ %{$reset_color%}'
+export PROMPT='%{$fg[green]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%}:%{$fg[cyan]%}%~%{$reset_color%}$(git_prompt_info) %{$reset_color%}
+%(?.%{$fg[green]%}.%{$fg[red]%})λ %{$reset_color%}'
 
-export GIT_PROMPT_PREFIX=" %{$fg[blue]%}on%{$reset_color%} %{$fg_bold[red]%}"
-export GIT_PROMPT_SUFFIX="%{$reset_color%}"
+export GIT_PROMPT_PREFIX="[%{$fg_bold[red]%}"
+export GIT_PROMPT_SUFFIX="%{$reset_color%}]"
 export GIT_PROMPT_DIRTY="*"
 export GIT_PROMPT_CLEAN=""
