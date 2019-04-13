c3d68524ba4ac04ceb92ef59a01e73df019dc526
Andrew Stewart <andrew@stwrt.ca>
2019-04-12 17:20:53 -0700

    Rename fuzzy finder helper

--- fuzzy/cli.zsh
+++ fuzzy/cli.zsh
@@ -1,11 +1,11 @@
-# Use fzy for finding paths
+# Use fuzzy finder for finding paths
 # By default, ^S freezes terminal output and ^Q resumes it. Disable that so
 # that those keys can be used for other things.
 unsetopt flowcontrol
 
-# Run fzy in the current working directory, appending the selected path, if
-# any, to the current command, followed by a space.
-function insert-fzy-path-in-command-line() {
+# Run fuzzy finder in the current working directory, appending the selected
+# path, if any, to the current command, followed by a space.
+function insert-fuzzy-path-in-command-line() {
     local selected_path
     # Print a newline or we'll clobber the old prompt.
     echo
@@ -18,7 +18,7 @@ function insert-fzy-path-in-command-line() {
 }
 
 # Create the zle widget
-zle -N insert-fzy-path-in-command-line
+zle -N insert-fuzzy-path-in-command-line
 
 # Bind the key to the newly created widget
-bindkey "^S" "insert-fzy-path-in-command-line"
+bindkey "^S" "insert-fuzzy-path-in-command-line"
